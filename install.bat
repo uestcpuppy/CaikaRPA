ab3b93f8be728f636d20775354f6af82ecb24f2f
uestcpuppy <118702510+uestcpuppy@users.noreply.github.com>
2022-12-28 17:57:02 +0800

    ll

--- install.bat
+++ install.bat
@@ -1,5 +1,13 @@
 @echo off
+%1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c pushd ""%~dp0"" && ""%~s0"" ::","","runas",1)(window.close)&&exit
+echo 设置python环境变量
+setx PATH "%PATH%;D:\caika\python37; D:\caika\python37\Scripts"
+echo 将mysql设置为系统服务
+D:\caika\mysql-8.0.31\bin\mysqld --install
+echo 启动mysql
+net start mysql
+echo 将启动脚本复制到桌面
 set d=%USERPROFILE%\Desktop
 copy D:\caika\rpa\caika.bat %d%
 echo 财咖网银助手安装完成,  请点击桌面caika图标启动系统!
-pause
+@pause
