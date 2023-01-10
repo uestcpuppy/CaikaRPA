6f5c8d514d888ea1fe3d5643d210104256b81d8c
uestcpuppy <118702510+uestcpuppy@users.noreply.github.com>
2023-01-10 15:20:54 +0800

    ll

--- install.bat
+++ install.bat
@@ -1,15 +1,20 @@
 @echo off
 %1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c pushd ""%~dp0"" && ""%~s0"" ::","","runas",1)(window.close)&&exit
-echo 修改注册表
+echo 【一】修改注册表
 regedit /s chrome.reg
-echo 设置python环境变量
+echo.
+echo 【二】设置python环境变量
 setx PATH "D:\caika\python37;D:\caika\python37\Scripts;%PATH%;" /m
-echo 将mysql设置为系统服务
+echo.
+echo 【三】将mysql设置为系统服务
 D:\caika\mysql-8.0.31\bin\mysqld --install
-echo 启动mysql
+echo.
+echo 【四】启动mysql
 net start mysql
-echo 将启动脚本复制到桌面
+echo.
+echo 【五】将启动脚本复制到桌面
 set d=%USERPROFILE%\Desktop
-copy D:\caika\rpa\caika.bat %d%
+copy D:\caika\rpa\*.lnk %d%
+echo.
 echo 财咖网银助手安装完成,  请点击桌面caika图标启动系统!
 @pause
