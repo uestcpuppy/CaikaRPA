bf4fdab7328ee52c698edb84f9484315371a3164
uestcpuppy <118702510+uestcpuppy@users.noreply.github.com>
2023-04-26 17:32:31 +0800

    ll

---
+++ config_goski.py
@@ -0,0 +1,33 @@
+PROJECT_ROOT = "C:\\Users\\44365\PycharmProjects\\bankRPA\\"
+# PROJECT_ROOT = "D:\\caika\\rpa\\"
+# DATA_ROOT = "D:\\data\\"
+DATA_ROOT = "D:\\caika\\data\\"
+
+DOWNLOAD_TEMP_DIR = DATA_ROOT + "temp\\"
+DOWNLOAD_DIR = DATA_ROOT + "download\\"
+UPLOAD_PATH =  DATA_ROOT + "upload\\"
+PORT_NUMBER_WEB = 8080
+PORT_NUMBER_DD = 8888
+
+IMPLICITLY_WAIT = 10
+SCRIPT_TIMEOUT = 10
+# PAGELOAD_TIMEOUT = 10
+
+ukey_dict = {}
+ukey_dict[1] = "95561e9002846938"
+ukey_dict[2] = "955668N014185053"
+ukey_dict[3] = "北京云动极限商贸有限公司"
+ukey_dict[4] = "955668N014185040"
+ukey_dict[5] = "北京贴地飞行体育文化有限公司"
+ukey_dict[6] = "955668N014185079"
+ukey_dict[7] = "0"
+ukey_dict[8] = "w0920014494600001.c.3100.0202"
+ukey_dict[9] = "0"
+ukey_dict[10] = "0"
+ukey_dict[11] = "9557718007157800"
+ukey_dict[12] = "J800028000175118"
+ukey_dict[13] = "955660Z023483690"
+ukey_dict[14] = "0"
+ukey_dict[15] = "w0900068050700001.c.0807.0202"
+ukey_dict[16] = "w0900067821600001.c.0807.0202"
+ukey_dict[17] = "J800028000601431"
---
+++ config_xiaolidu.py
@@ -0,0 +1,42 @@
+#PROJECT_ROOT = "C:\\Users\\44365\PycharmProjects\\bankRPA\\"
+PROJECT_ROOT = "G:\\caika\\rpa\\"
+# DATA_ROOT = "D:\\data\\"
+DATA_ROOT = "G:\\caika\\data\\"
+
+DOWNLOAD_TEMP_DIR = DATA_ROOT + "temp\\"
+DOWNLOAD_DIR = DATA_ROOT + "download\\"
+UPLOAD_PATH =  DATA_ROOT + "upload\\"
+PORT_NUMBER_WEB = 8080
+PORT_NUMBER_DD = 8888
+
+IMPLICITLY_WAIT = 10
+SCRIPT_TIMEOUT = 10
+# PAGELOAD_TIMEOUT = 20
+
+ukey_dict = {}
+ukey_dict[1] = "9558059000410193"
+ukey_dict[2] = "GSB00029025918"
+ukey_dict[3] = "BJRCBE8113341941"
+ukey_dict[4] = "BJRCBE8314328993"
+ukey_dict[5] = "B06423120"
+ukey_dict[6] = "GSB00005248325"
+ukey_dict[7] = "GSB00010312511"
+ukey_dict[8] = "GSB00010312510"
+ukey_dict[9] = "GSB00024427371"
+ukey_dict[10] = "GSB00029025452"
+ukey_dict[11] = "GSB00024427391"
+ukey_dict[12] = "GSB00024427275"
+ukey_dict[13] = "GSB00029025337"
+ukey_dict[14] = "9558059001383983"
+ukey_dict[15] = "9558059000410031"
+ukey_dict[16] = "9558059003250716"
+ukey_dict[17] = "9558059003250763"
+ukey_dict[18] = "9558059001570878"
+ukey_dict[19] = "9558059000408750"
+ukey_dict[20] = "9558059001377823"
+ukey_dict[21] = "9558059001383955"
+ukey_dict[22] = "GSB00030258481"
+ukey_dict[23] = "9558059000395601"
+ukey_dict[24] = "9558059000204935"
+ukey_dict[25] = "9558059001377892"
+ukey_dict[26] = "9558059002320410"
